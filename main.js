a5e4da7eccf05e3bde2bac3dc6b2a70cbb6a00ab
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-09-12 14:32:34 -0400

    number buttons able to display on calc display area

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//Start up and initializing operations
+function giveEventNums() {
+    let items = document.querySelectorAll(`.num-color`);
+    items.forEach(function(item) {
+        item.onclick = calcNumButton;
+    })
+}
+giveEventNums();
+
+
+//Calculator button functionality
+function calcNumButton(event) {
+    pressNum = event.target;
+    document.querySelector(`#calc-display`).innerText += pressNum.innerText;
+}
+
+
 // Start right-main JS
 function clearRightDisplay() {
     document.querySelector(`#right-main-bottom`).innerText = ``;
