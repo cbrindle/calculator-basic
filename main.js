988988a5638793cd652690c1d478e0e271e1baf0
Christopher Brindle <chris.brindle@codeimmersives.com>
2019-09-15 11:24:36 -0400

    added event listener to document to allow number key presses to function the same as clicking the number buttons

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 //Calculator button functionality//
 ///////////////////////////////////
+document.addEventListener(`keydown`, typeNum);
+
+function typeNum(event) {
+    const nums = {
+                    Digit1: `1`,
+                    Digit2: `2`,
+                    Digit3: `3`,
+                    Digit4: `4`,
+                    Digit5: `5`,
+                    Digit6: `6`,
+                    Digit7: `7`,
+                    Digit8: `8`,
+                    Digit9: `9`,
+                    Digit0: `0`,
+                }
+    if (event.code in nums) {
+        if (document.querySelector(`#calc-number-area`).innerText === `0`) {
+            document.querySelector(`#calc-number-area`).innerText = ``;
+        }
+        document.querySelector(`#calc-number-area`).innerText += nums[event.code];
+        if (Number(document.querySelector(`#calc-number-area`).innerText) > 0) {
+            document.querySelector(`.acButton`).innerText = `C`;
+        } else {
+            document.querySelector(`.acButton`).innerText = `AC`;
+        }
+    }
+}
+
 function calcNumButton(event) {
     pressNum = event.target;
     if (document.querySelector(`#calc-number-area`).innerText === `0`) {
